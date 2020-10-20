ed0e04defa5ad880629528e3451b42db8382c655
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2020-10-20 18:10:46 -0400

    with rtc

--- renderer.js
+++ renderer.js
@@ -6,8 +6,12 @@
 // process.
 
 var list = document.getElementById("selectList");
+
 var refreshBtn = document.getElementById("refreshBtn");
 var captureBtn = document.getElementById("captureBtn");
+
+var sourceVideo = document.querySelector('#source');
+var outputVideo = document.querySelector('#output');
 
 refreshSources();
 
@@ -37,20 +41,64 @@ async function refreshSources() {
 async function doCapture() {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
-          audio: false,
-          video: {
-            mandatory: {
-              chromeMediaSource: 'desktop',
-              chromeMediaSourceId: list.value,
-            }
-          }
-        })
+      audio: false,
+      video: {
+        mandatory: {
+          chromeMediaSource: 'desktop',
+          chromeMediaSourceId: list.value,
+        }
+      }
+    });
 
-    const video = document.querySelector('video')
-    video.srcObject = stream
-    video.onloadedmetadata = (e) => video.play()
+    const video = document.querySelector('video');
+    video.srcObject = stream;
+    video.onloadedmetadata = (e) => video.play();
+
+    startPeerConnection(stream);
   } catch (e) {
     console.error(e);
   }
 }
 
+function startPeerConnection(stream) {
+    console.log(stream);
+
+    window.RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection ||
+                               window.mozRTCPeerConnection;
+
+    var yourConnection = new RTCPeerConnection();
+    var theirConnection = new RTCPeerConnection();
+
+    yourConnection.addStream(stream);
+    theirConnection.onaddstream = function(e) {
+        outputVideo.srcObject = e.stream;
+    };
+
+    yourConnection.onicecandidate = function (event) {
+        if (event.candidate) {
+            theirConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+        }
+    };
+
+    theirConnection.onicecandidate = function (event) {
+        if (event.candidate) {
+            yourConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+        }
+    }
+
+    yourConnection.createOffer(function (offer) {
+
+        yourConnection.setLocalDescription(offer);
+        theirConnection.setRemoteDescription(offer);
+
+        theirConnection.createAnswer(function (offer) {
+
+            theirConnection.setLocalDescription(offer);
+            yourConnection.setRemoteDescription(offer);
+        }, function (error) {
+            console.log(error);
+        });
+    }, function (error) {
+        console.log(error);
+    });
+}
